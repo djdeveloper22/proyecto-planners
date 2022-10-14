df37f8c1989d0410e2375f836642c952bc7fefa8
djdeveloper22 <djdeveloper@gmail.com>
2022-10-13 22:27:44 -0500

    se crea la seccion de compra que se despliega en el lateral derecho

--- main.js
+++ main.js
@@ -4,12 +4,25 @@ const btnCarrito = document.querySelector("#btnCarrito");
 let contadorCart = document.querySelector("#contadorCart");
 let container_card = document.querySelector(".container-card");
 let contenedor_card = document.querySelector(".container__card");
+const carrito = document.querySelector('#carrito');
+const contenedorProductos = document.querySelector('#contenedor__productos');
+const cerrar = document.querySelector('#cerrar');
 const aProductos = [];
 let cont = 0;
 
+
+carrito.addEventListener('click', ()=>{
+  contenedorProductos.style.right = '0';
+  
+});
+
+cerrar.addEventListener('click', ()=>{
+  contenedorProductos.style.right = '-375px';
+});
 btnCarrito.addEventListener("click", (e) => {
   e.preventDefault();
   cont++;
   contadorCart.textContent = +`${cont}`;
   alert('producto agregado');
 });
+
